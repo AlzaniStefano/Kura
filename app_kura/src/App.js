4f8ba2f4f1b485ec8b9479e61440a994c86a4ced
Stefano Alzani <alzani.stefano96@gmail.com>
2024-04-22 19:12:48 +0200

    strapi react webpack config

---
+++ app_kura/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('http://localhost:1337/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Errore durante la chiamata API:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista dei Post</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
